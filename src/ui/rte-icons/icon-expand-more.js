0215482e8fd051ac205545986cc4b0abc49e75d7
AUTOTRADER\dpamidi <divya.pamidi@vinsolutions.com>
2016-08-16 16:36:34 -0500

    adding dropdown icon

--- src/ui/rte-icons/icon-expand-more.js
+++ src/ui/rte-icons/icon-expand-more.js
@@ -3,10 +3,9 @@ import React from 'react';
 class IconExpandMore extends React.Component {
     render() {
         return (
-            <svg width="12" height="12" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M17.4,8.1c0.4-0.5,1.1-0.5,1.5,0c0.4,0.5,0.4,1.3,0,1.8l-6.2,7c-0.4,0.5-1.1,0.5-1.5,0l-6.2-7c-0.4-0.5-0.4-1.3,0-1.8
-                c0.4-0.5,1.1-0.5,1.5,0l5.4,5.8L17.4,8.1z"/>
-
+            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 10l5 5 5-5z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
             </svg>
         )}
 }
